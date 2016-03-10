f5d65a1a0cbe2be2cd614e64d14e64954a0a1368
davidadkins1 <davidadkins1@msn.com>
2016-03-10 09:43:46 -0700

    debug

--- R1D1_ScratchX/arduino_extension.js
+++ R1D1_ScratchX/arduino_extension.js
@@ -180,10 +180,10 @@
       case CAPABILITY_RESPONSE:
         for (var i = 1, pin = 0; pin < MAX_PINS; pin++) {
             while (storedInputData[i++] != 0x7F) {
-                pinModes[storedInputData[i - 1]].push(pin);
-                console.log('Test ' + storedInputData[i-1]);
+                console.log('Test ' + storedInputData[i - 1]);
                 console.log(' ' + pinModes[storedInputData[i - 1]]);
                 console.log(' ' + pinModes.length);
+                pinModes[storedInputData[i - 1]].push(pin);
             i++; //Skip mode resolution
           }
           if (i == sysexBytesRead) break;
